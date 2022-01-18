9fdecd7cc82795d17af3170c7b48808ecc5550dc
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2022-01-18 16:06:36 +0800

    Initial Commit

---
+++ World Time/World_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_TimeApp.swift
+//  World Time
+//
+//  Created by Beatrice Totanes on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct World_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
