0192581e1328f52865efb66f85a638efe3edd334
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2022-01-18 18:30:12 +0800

    Finished text view templates

--- World Time/Views/TextView.swift
+++ World Time/Views/TextView.swift
@@ -7,14 +7,61 @@
 
 import SwiftUI
 
+struct RoundedeRectText: View {
+  var text: String
+  
+  var body: some View {
+    SmallTextSemiBold(text: text)
+      .font(.headline)
+      .overlay(
+        RoundedRectangle(cornerRadius: 10)
+          .stroke(lineWidth: 1.0)
+          .frame(width: 124, height: 39)
+      )
+  }
+}
+
+struct SmallTextSemiBold: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .font(.headline)
+  }
+}
+
+
+struct SmallText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .font(.body)
+  }
+}
+
+struct MainText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .font(.system(size: 70))
+      .fontWeight(.bold)
+  }
+}
+
 struct TextView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    VStack{
+      RoundedeRectText(text: "Philippines")
+      MainText(text: "10:00 PM")
+      SmallText(text: "Greece")
     }
+  }
 }
 
 struct TextView_Previews: PreviewProvider {
-    static var previews: some View {
-        TextView()
-    }
+  static var previews: some View {
+    TextView()
+  }
 }
